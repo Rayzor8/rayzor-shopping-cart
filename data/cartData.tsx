071bc8acf2d37ea8fafc35efce6678fa24357255
flashback220 <rayhidayat220@gmail.com>
2022-12-03 10:20:59 +0700

    upload images and set up data

--- data/cartData.tsx
+++ data/cartData.tsx
@@ -1,48 +1,74 @@
+import { StaticImageData } from "next/image";
+import { fade, autotronic, gammaDoppler } from "./imagesData/knifes";
+import {
+  bloodSport,
+  fuelInjector,
+  legionOfAnubis,
+  oniTaiji,
+  theEmpress,
+} from "./imagesData/rifles";
+
 type CartData = {
-  weaponName: string;
+  weaponNames: WeaponNames[];
   weaponType: string;
-  weaponMiniImg: string;
-  weaponLargeImg: string;
-  weaponPricesList: PriceList[];
 };
 
-type PriceList = {
-  condition: string;
-  price: string;
+type WeaponNames = {
+  name: string;
+  img: StaticImageData;
+  price: number;
+  // imgLarge:StaticImageData;
 };
 
 const data: CartData[] = [
   {
-    weaponName: "AK47",
-    weaponType: "rifle",
-    weaponMiniImg: "1.jpg",
-    weaponLargeImg: "2.jpg",
-    weaponPricesList: [
-      { condition: "factory new", price: "$300" },
-      { condition: "minimal wear", price: "$100" },
-      { condition: "battle scarred", price: "$50" },
+    weaponType: "Knife",
+    weaponNames: [
+      {
+        name: "Fade",
+        img: fade,
+        price: 200,
+      },
+      {
+        name: "Autotronic",
+        img: autotronic,
+        price: 300,
+      },
+      {
+        name: "GammaDoppler",
+        img: gammaDoppler,
+        price: 100,
+      },
     ],
   },
   {
-    weaponName: "AK47",
-    weaponType: "rifle",
-    weaponMiniImg: "1.jpg",
-    weaponLargeImg: "2.jpg",
-    weaponPricesList: [
-      { condition: "factory new", price: "$300" },
-      { condition: "minimal wear", price: "$100" },
-      { condition: "battle scarred", price: "$50" },
-    ],
-  },
-  {
-    weaponName: "AK47",
-    weaponType: "rifle",
-    weaponMiniImg: "1.jpg",
-    weaponLargeImg: "2.jpg",
-    weaponPricesList: [
-      { condition: "factory new", price: "$300" },
-      { condition: "minimal wear", price: "$100" },
-      { condition: "battle scarred", price: "$50" },
+    weaponType: "Rifle",
+    weaponNames: [
+      {
+        name: "Blood sport",
+        img: bloodSport,
+        price: 50,
+      },
+      {
+        name: "Fuel injector",
+        img: fuelInjector,
+        price: 100,
+      },
+      {
+        name: "Legion of anubis",
+        img: legionOfAnubis,
+        price: 70,
+      },
+      {
+        name: "Oni tai ji",
+        img: oniTaiji,
+        price: 90,
+      },
+      {
+        name: "The empress",
+        img: theEmpress,
+        price: 100,
+      },
     ],
   },
 ];
